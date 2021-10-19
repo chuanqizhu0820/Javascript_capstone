4d9a82ed696dcdda5dd84bc53600645f2eb32489
chuanqizhu0820 <chuanqizhu1991@outlook.com>
2021-10-19 16:21:28 +0100

    Add like functionality

--- src/index.js
+++ src/index.js
@@ -1,15 +1,83 @@
 import './style.css';
 
+let appId = "K5LEyqREMBDZLL96ZFuw"
+
+const addLike = async (item_id) => {
+
+    const response = await fetch("https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/K5LEyqREMBDZLL96ZFuw/likes", {
+        method: 'POST',
+        body: JSON.stringify(
+            {
+                "item_id": item_id
+            }),
+        headers: {
+            'Content-type': 'application/json; charset=UTF-8',
+        },
+    })
+    response.json().then((json) => {
+        console.log(json);
+    });
+}
+
+const getLike = (id, node) => {
+    console.log(node);
+    fetch("https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/K5LEyqREMBDZLL96ZFuw/likes")
+        .then(response => response.json())
+        .then(json => {
+            let bool = false
+            let idx = 0;
+            let likenum = -1;
+            json.forEach((item, i) => {
+                if (item.item_id === id) {
+                    bool = true;
+                    idx = i;
+                    likenum = item.likes;
+                }
+            })
+            if (bool) {
+                node.textContent = `like ${likenum + 1}`;
+                addLike(id);
+            } else if (bool == false) {
+                node.textContent = `like 1`;
+                addLike(id);
+            }
+        });
+}
+
+const loadLike = (id, node) => {
+    console.log(node);
+    fetch("https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/K5LEyqREMBDZLL96ZFuw/likes")
+        .then(response => response.json())
+        .then(json => {
+            let bool = false
+            let idx = 0;
+            let likenum = 0;
+            json.forEach((item, i) => {
+                if (item.item_id === id) {
+                    bool = true;
+                    idx = i;
+                    likenum = item.likes;
+                }
+            })
+            if (bool) {
+                node.textContent = `like ${likenum}`;
+            } else if (bool == false) {
+                node.textContent = `like 0`;
+            }
+        });
+}
+
+
+
 const getFood = async () => {
     const response = await fetch("https://www.themealdb.com/api/json/v1/1/filter.php?a=Chinese");
     response.json().then((json) => {
         let itemArr = json.meals;
-        console.log(itemArr[0]);
         let itemHtml = '';
         itemArr.forEach(item => {
             itemHtml +=
                 `
-                <div id="${item.idMeal}" class="col-4">
+                <div class="col-4">
                 <div class="row">
                 <div class="col-12 item-img">
                 <img src=${item.strMealThumb} alt="item image">
@@ -17,8 +85,8 @@ const getFood = async () => {
                 <div class="col-12 text-center">
                 <p>${item.strMeal}</p>
                 </div>
-                <div class="col-6">
-                <img src="https://img.icons8.com/material-outlined/24/000000/like--v2.png" />
+                <div class="col-6 item-like">
+                <img id="${item.idMeal}" src="https://img.icons8.com/material-outlined/24/000000/like--v2.png" />
                 <span>likes</span>
                 </div>
                 <div class="col-6 text-left">
@@ -30,10 +98,28 @@ const getFood = async () => {
 
         const itemConstainer = document.querySelector("#items");
         itemConstainer.innerHTML = itemHtml;
+
+        const itemLike = document.querySelectorAll(".item-like > img");
+        itemLike.forEach((item) => {
+            let likeNode = item.parentNode.querySelector("span");
+            loadLike(item.id, likeNode);
+            item.addEventListener("click", () => {
+                let likeNode = item.parentNode.querySelector("span");
+                getLike(item.id, likeNode);
+            })
+        })
     });
 };
 
 getFood();
+
+
+
+
+
+
+
+
 
 
 
