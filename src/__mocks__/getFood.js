9dc301d56f5f519c6bcd642b87e9eb7947057ca9
chuanqizhu0820 <chuanqizhu1991@outlook.com>
2021-10-20 10:44:36 +0100

    Add header and test the meal number

--- src/__mocks__/getFood.js
+++ src/__mocks__/getFood.js
@@ -1,11 +1,5 @@
-const getFood = async () => {
-    const response = await fetch("https://www.themealdb.com/api/json/v1/1/filter.php?a=Chinese");
-    response.json().then((json) => {
-        let itemArr = json.meals;
-        const dishNum = document.querySelector("#dish-num");
-        console.log(itemArr.length);
-        return itemArr.length;
-    })
-};
+const getFood = (foodArr) => {
+    return foodArr.length;
+}
 
 module.exports = getFood;
--- src/__tests__/getFood.test.js
+++ src/__tests__/getFood.test.js
@@ -1,6 +1,11 @@
-
 const getFood = require('../__mocks__/getFood');
 
-test("should return 12 dishes", () => {
-    expect(getFood()).toBe(12);
+test("should return 3 dishes", () => {
+    foodArr = ["food1", "food2", "food3"];
+    expect(getFood(foodArr)).toBe(3);
+})
+
+test("should return 6 dishes", () => {
+    foodArr = ["food1", "food2", "food3", "food4", "food6", "food6"];
+    expect(getFood(foodArr)).toBe(6);
 })
